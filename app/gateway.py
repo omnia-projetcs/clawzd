0f6ff64cc76fa259b59c113fe950728a23dd5390
nico <nico248@free.fr>
2026-05-08 23:41:42 +0200

    chore: suppress logging output from websockets library in gateway configuration

--- app/gateway.py
+++ app/gateway.py
@@ -57,6 +57,8 @@ import httpx
 logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(name)s] %(message)s")
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logging.getLogger("httpcore").setLevel(logging.WARNING)
+logging.getLogger("websockets").setLevel(logging.WARNING)
+logging.getLogger("websockets.server").setLevel(logging.WARNING)
 
 logger = logging.getLogger("clawzd.gateway")
 
